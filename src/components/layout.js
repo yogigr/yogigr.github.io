020e937e1e9297c6630e605d88c350cfde3602d7
Yogi Gilang Ramadhan <yogigilang182@gmail.com>
2021-09-24 18:29:14 +0700

    modified profile

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <Link to="/">yogigr</Link>
       </footer>
     </div>
   )
